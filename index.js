a4de0c46f6f735e627625e504dd97c107d26a720
Angel <rachel@Rachels-MacBook-Air.local>
2023-03-08 01:08:37 +0000

    file structure and build function

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("./src/modules/Manager");
+const Engineer = require("./src/modules/Engineer");
+const Intern = require("./src/modules/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const questions = require("./questions.js")
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-//const render = require("./src/page-template.js");
+const render = require("./src/page-template.js");
 
 
 // consider adding validation to ensure that user input is in the proper format. 
-
-// ask qs inquirer. 
-// is this going to be an internal, manager or Engineer, our output needs to create an array. The array consists of output from enquirer. user selected makes new manager. 
 
 let team = [];
 
@@ -31,7 +28,6 @@ async function startProgram() {
 }
 
 async function menu() {
-  //ask
   let menuAnswer = await inquirer.prompt(questions.menu)
 
   switch (menuAnswer.action) {
@@ -42,13 +38,10 @@ async function menu() {
       addIntern()
       break;
     default:
-      console.log("Supposed to write the file")
+      finishBuild()
       break;
   }
-
-
 }
-
 
 async function addEngineer() {
   let data = await inquirer
@@ -63,7 +56,6 @@ async function addEngineer() {
       menu()
     }
 
-
 async function addIntern() {
   let data = await inquirer
     .prompt(questions.intern)
@@ -73,40 +65,15 @@ async function addIntern() {
     data.id,
     data.email,
     data.school)
+    team.push(intern)
   menu()
 }
 
-// // Finish Building Team Option:
-// // Exit application, HTML generated. 
-
-
-
-// // team.push engineerResponse, internResponse, managerResponse 
-
-
-// // Render function CAlled, passing in array of employee objects. 
-
-
-// // render function includes block of HTML including templated divs for each employee!
-
-
-// // * Create an HTML file using the HTML returned from the `render` function. 
-// //   * Write it to a file named `team.html` in the `output` folder. 
-// //   * You can use the provided variable `outputPath` to target this location.
-
-
-// team.push(new Manager(Angel, 12, "my.email@email.com"))
-// team.push(new Engineer(Richie, 8, "his@email.net"))
-// //  these values come from inquirer. 
-// // are you done, then after all filled out. then render, pas team to render. 
-// // then it generates the html doc based on the array 
-
-// let htmlDoc = render(team)
-
-// // need to us fs write file. render returns a string the string is what we want to write to the html and that write to file. 
-
-// await fs.writeFile(outputPath, htmlDoc)
-
+async function finishBuild(){
+  let htmlDoc = render(team)
+  await fs.writeFile(outputPath, htmlDoc)
+  console.log('Complete')
+}
 
 
 startProgram()
